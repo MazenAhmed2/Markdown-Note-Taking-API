6872fa51519e309c31f1c124c27aa9947fa96be7
Mazen Ahmed <106814868+MazenAhmed2@users.noreply.github.com>
2025-04-29 12:35:22 +0000

    adding notes/:id

--- controllers/notes.js
+++ controllers/notes.js
@@ -8,5 +8,18 @@ export default {
       console.log(err)
       res.status(500).json({success : false})
     }
-  }
+  }, 
+
+  getNoteById : async function (req, res){
+    try {
+      // Parse note ID
+      const id = req.params.id 
+      
+      // Send response
+      res.status(200).json((await model.find({_id : id}))[0])
+    } catch(err){
+      console.log(err)
+      res.status(500).json({success : false})
+    }
+  }, 
 }
--- routes/notes.js
+++ routes/notes.js
@@ -4,6 +4,9 @@ import controller from '../controllers/notes.js'
 const router = express.Router()
 
 router.get('/', controller.getAllNotes)
+router.get('/:id', controller.getNoteById)
+// router.get('/', controller.getAllNotes)
+// router.get('/', controller.getAllNotes)
 
 
 export default router
